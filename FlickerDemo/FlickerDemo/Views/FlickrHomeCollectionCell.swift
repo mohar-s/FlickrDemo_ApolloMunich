800b73009197bd9e11fdda9c7e00b5508ac3d0c1
mohar-singh <moharsinghindia65@gmail.com>
2019-02-07 10:01:30 +0530

    Added project in git repositry
    Upload project

---
+++ FlickerDemo/FlickerDemo/Views/FlickrHomeCollectionCell.swift
@@ -0,0 +1,16 @@
+//
+//  FlickrHomeCollectionCell.swift
+//  FlickerDemo
+//
+//  Created by RailYatri on 06/02/19.
+//  Copyright © 2019 MoharSingh. All rights reserved.
+//
+
+import UIKit
+
+class FlickrHomeCollectionCell: UICollectionViewCell {
+    
+    @IBOutlet weak var flickrIv: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+}
